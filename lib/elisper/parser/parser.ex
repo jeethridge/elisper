ce15823b91dc2f8e28c70209e6b8864281a76e87
James Ethridge <jeethridge@gmail.com>
2019-03-21 20:08:42 -0400

    parser passing basic smoke test

--- lib/elisper/parser/parser.ex
+++ lib/elisper/parser/parser.ex
@@ -2,11 +2,13 @@ defmodule Elisper.Parser.Parser do
   @moduledoc """
   Defines the parsing operations required for the interpreter to work.
   """
-
+  alias Elisper.Parser.Tokenizer
+  alias Elisper.Parser.AST
   @doc """
-  Read a schem expression from a string.
+  Reads a scheme expression from a string and returns the abstract syntax tree.
   """
   def parse(program) do
-    nil
+    {:ok, [], ast} = program |> Tokenizer.tokenize() |> AST.build([])
+    ast
   end
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -1,8 +1,12 @@
 defmodule ParserTest do
   use ExUnit.Case
 
-  @tag :ignore
-  test "stub" do
-    assert true
+  alias Elisper.Parser
+
+  test "a valid scheme expression results in a valid AST" do
+    expression = "(begin (define x 10) (* 3 ( + x 10 ))"
+    expected = [":begin", [":define", "x", 10], [":*", 3, [":+", "x", 10]]]
+    ast = Parser.parse(expression)
+    assert ast == expected
   end
 end
