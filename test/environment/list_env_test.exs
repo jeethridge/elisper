f2e588204d866cd7a2a77b3da0eba208f7ae0df9
James Ethridge <jeethridge@gmail.com>
2019-03-24 18:47:10 -0400

    implements make_frame

--- test/environment/list_env_test.exs
+++ test/environment/list_env_test.exs
@@ -6,4 +6,13 @@ defmodule ListEnvTest do
   test "empty environment is empty list" do
     assert ListEnv.empty == []
   end
+
+  test "make_frame builds a pair of lists" do
+    variables = ["x", "y", "z"]
+    values = [1, 2, 3]
+    expected_frame = [["x", "y", "z"], [1, 2, 3]]
+    frame = ListEnv.make_frame(variables, values)
+    assert frame == expected_frame
+  end
+
 end
